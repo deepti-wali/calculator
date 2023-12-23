1a74553f5516e985090672280c0fce473549dcba
Deepti <134826480+deepti-wali@users.noreply.github.com>
2023-12-23 16:17:59 +0530

    first commit

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,63 +1,10 @@
 // import React, { useState } from "react";
 import "./Calculator.css";
 
-// const Calculator = () => {
-//   const [input, setInput] = useState("");
-//   const [output, setOutput] = useState("");
-
-//   const handleClick = (value) => {
-//     setInput((prevInput) => prevInput + value);
-//   };
-
-//   const handleReset = () => {
-//     setInput("");
-//     setOutput("");
-//   };
-
-//   const handleEvaluate = () => {
-//     try {
-//       const result = eval(input);
-//       setOutput(`${result}`);
-//     } catch (error) {
-//       setOutput("Error");
-//     }
-//   };
-
-//   return (
-//     <div className="calculator" style={{ textAlign: "center" }}>
-//       <input type="text" value={input} style={{}}></input>
-//       <div className="result">{output && <p>{output}</p>}</div>
-//       <br />
-//       <div className="buttons">
-//         <button onClick={() => handleClick("7")}>7</button>
-//         <button onClick={() => handleClick("8")}>8</button>
-//         <button onClick={() => handleClick("9")}>9</button>
-//         <button onClick={() => handleClick("+")}>+</button>
-
-//         <button onClick={() => handleClick("4")}>4</button>
-//         <button onClick={() => handleClick("5")}>5</button>
-//         <button onClick={() => handleClick("6")}>6</button>
-//         <button onClick={() => handleClick("-")}>-</button>
-
-//         <button onClick={() => handleClick("1")}>1</button>
-//         <button onClick={() => handleClick("2")}>2</button>
-//         <button onClick={() => handleClick("3")}>3</button>
-//         <button onClick={() => handleClick("*")}>*</button>
-
-//         <button onClick={handleReset}>C</button>
-//         <button onClick={() => handleClick("0")}>0</button>
-//         <button onClick={handleEvaluate}>=</button>
-//         <button onClick={() => handleClick("/")}>/</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Calculator;
 import React, { useState } from "react";
 import "./App.css";
 
-const App = () => {
+const Calculator = () => {
   const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
 
@@ -107,4 +54,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Calculator;
